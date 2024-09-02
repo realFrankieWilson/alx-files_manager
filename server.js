11c12541ce38d8a5f881136328bf2bad0b518c37
Loubnaa1 <loubnamerzak4@gmail.com>
2024-09-02 23:12:42 +0000

    my work

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-hello
+const express = require('express');
+const routes = require('./routes/index'); // Import routes
+
+const app = express();
+const port = process.env.PORT || 5000; // Use environment variable PORT or default to 5000
+
+app.use('/', routes); // Use the imported routes
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
